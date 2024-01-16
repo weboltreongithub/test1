ac3fa2725c25909bb2962a33f46db3f228822914
David <david.lomuscio@gmail.com>
2024-01-16 12:40:08 +0100

    Update line-counter.sh

--- script/line-counter.sh
+++ script/line-counter.sh
@@ -1,5 +1,5 @@
 a_number=$(cat *.cpp | wc -l)
 
-sed  -e "s/__LINES__/${a_number}/" report-template.md > report.md
+sed  -e "s/__LINES__/${a_number}/" script/report-template.md > report.md
 
 echo "New report created"
